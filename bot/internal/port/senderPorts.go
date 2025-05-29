1c107e52d8ccf00d138b6d1c7e25a5b4d0b1b1a3
Kirill Zenuk <relkos12tyuiop@gmail.com>
2025-05-29 20:00:12 +0400

    fix port

--- bot/internal/port/senderPorts.go
+++ bot/internal/port/senderPorts.go
@@ -4,10 +4,10 @@ import "os"
 
 //FileSender is port for sending file
 type FileSender interface {
-	SendFile(chatID int64, file *os.File)
+	SendFile(chatID int64, file *os.File) error
 }
 
 //MessageSender is port for sending message
 type MessageSender interface {
-	SendMessage(chatID int64, text string)
+	SendMessage(chatID int64, text string) error
 }
