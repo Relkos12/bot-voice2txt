3c194622aa5ce1d4191099104ff9c21bae5b121b
Kirill Zenuk <relkos12tyuiop@gmail.com>
2025-05-29 22:18:24 +0400

    created a config

---
+++ bot/internal/config/config.go
@@ -0,0 +1,26 @@
+package config
+
+import (
+	"errors"
+	"log/slog"
+	"os"
+)
+
+// BOT_TOKEN_ENV is environment variable
+const BOT_TOKEN_ENV = "BOT_TOKEN"
+
+type Config struct {
+	Token string
+}
+
+func LoadConfig(Logger *slog.Logger) (*Config, error) {
+
+	botToken := os.Getenv(BOT_TOKEN_ENV)
+
+	if botToken == "" {
+		Logger.Error("Requred env variables are misssing", "BOT_TOKEN", botToken)
+		return nil, errors.New("Requred env variables are missing")
+	}
+
+	return &Config{Token: botToken}, nil
+}
